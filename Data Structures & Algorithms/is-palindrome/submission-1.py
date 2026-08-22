eb6786162443c5d859abc25b929df19dc6168a0d
vansh <vanshsinhaacademics@gmail.com>
2026-08-21 22:33:54 -0700

    Update: is-palindrome - submission-1

--- Data Structures & Algorithms/is-palindrome/submission-1.py
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -1,18 +1,39 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        cleaned = "".join(char for char in s if char.isalnum())
-        left , right = 0, len(cleaned) - 1
 
+        
+        '''  
+        main idea:
+        1. start with a cleaned text where its just lowercase letters with no spaces or characters
+
+        2. initiate pointers to track each letter from opposite ends 
+
+        3. check to see that every left value matches right value and if it does then increment left    forward by 1 and right backward by 1 to meet at the middle and the while loop prevents them from overlapping
+
+           
+        
+        '''
+        
+        clean_text = "".join(char for char in s if 
+        char.isalnum()).lower()
+
+        left, right = 0, len(clean_text) - 1
+
+        # for each i in s:
+        
         while left < right:
-            if cleaned[left].lower() != cleaned[right].lower():
-                return False
-            # move right back and left forward
-            right -= 1
-            left += 1
+            if clean_text[left] == clean_text[right]:
+                left += 1
+                right -= 1
+                
+            else: 
+                return False 
         return True
 
 
 
-        
+
+
+
 
         
